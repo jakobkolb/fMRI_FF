fixed averages of delay and baseline timing

trial_structure.py
@@ -11,23 +11,43 @@ trial.ts = 1.5                                                  #time for one fm
 number_of_trials = 100                                          #obviously the number of trails to be taken
 spacing = [(-0.5,-0.2),(0.5,-0.2),(-0.5,0.6),(0.5,0.6),(0,0)]   #positions for objects on slides
                                                                 #marker_1, marker_2, stim_1, stim_2, fixation_cross
-
+desired_delay_average = 2
+desired_baseline_average = 3
 #data arrays for trail parameters and user input and reaction time
 output = np.zeros((number_of_trials,10))
 
 
 
-#the following variables have to be set for each trial separately. More later.
+#the following block sets up the timing and positioning of the trial features
 #-----------------------------------------------------------------------------
+print 'trial setup'
+
+
 #time for slides A, B, C, D
 timing = np.zeros((number_of_trials,4))
 timing[:,0:4] = [3,2,1,2]
 for i in range(number_of_trials):
     timing[i,1] = np.random.randint(1,5)
     timing[i,3] = np.random.randint(2,5)
+#adjust means of timing for delay and baseline slides such 
+Estimated_delay_time  = sum(timing[:,1])/number_of_trials
+n=1
+print Estimated_delay_time*number_of_trials, M
+while n<M:
+    i = np.random.randint(number_of_trials-1)
+    if timing[i,1]-1!=timing[i+1,1] and timing[i,1]-1!=timing[i+1,1] and timing[i,1]-1>0:
+        timing[i,1]-=1
+        n+=1
 
-print sum(timing[:,1])/number_of_trials
-
+Estimated_baseline_time  = sum(timing[:,3])/number_of_trials
+M   = int(number_of_trials*(Estimated_baseline_time - desired_baseline_average))
+n=1
+while n<M:
+    i = np.random.randint(number_of_trials-1)
+    if timing[i,3]-1!=timing[i+1,3] and timing[i,3]-1!=timing[i+1,3] and timing[i,3]-2>0:
+        timing[i,1]-=1
+        n+=1
+ 
 #difficulties for the current trial
 difficulties =  np.zeros((number_of_trials,2))
 difficulties[:,0:2] = [0.1, 0.3]
@@ -40,12 +60,17 @@ for i in range(number_of_trials):
     inversions[i,1] = np.random.randint(0,1)
     inversions[i,2] = np.random.randint(0,1)
 
+print 'mean delay time is ', sum(timing[:,1])/number_of_trials
+print 'mean baseline time is ', sum(timing[:,3])/number_of_trials
+
 #-----------------------------------------------------------------------------
 
 
+#-----------------------------------------------------------------------------
+#run the actual trials and record user input
+#-----------------------------------------------------------------------------
 win = visual.Window()
 
-#run trials
 
 for i in range(0,number_of_trials):
 
@@ -57,5 +82,11 @@ for i in range(0,number_of_trials):
     output[i,7:10]  = inversions[i,:]
 
 win.close()
+#-----------------------------------------------------------------------------
+
+
+#-----------------------------------------------------------------------------
+#evaluate user input and save relevant data
+#-----------------------------------------------------------------------------
 
 np.savetxt('experiment_output_data.tsv', output)