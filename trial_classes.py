c40eccd2b8b4e2343023ce348bd56d4cfb965ca7
jakobkolb <jkolb@physik.hu-berlin.de>
2014-03-19 17:41:24 +0100

    implemented user input evaluation in terms of easy and hard and display of random dot motion

--- trial_classes.py
+++ trial_classes.py
@@ -39,12 +39,15 @@ class trial:
         self.time_delay_2   = timing[3]*trial.ts  #display time for baseline
         trial.trialCount    += 1
 #define different slides for the trial (A: Stimulus, B: Delay, C: Decision, D: Baseline)
+    def getData(self):
+        return self.name, self.user_input[0], self.user_input[1],self.user_input[2],self.user_input[3] , self.tA, self.tB, self.tC, self.tD
 
-    def define_slide_A(self):
-        if self.flip[0] == 0:
+
+    def present_slide_A(self):
+        if self.flip[0] == 1:
             mpos1 = self.pos_marker_1
             mpos2 = self.pos_marker_2
-        elif self.flip[0] == 1:
+        elif self.flip[0] == -1:
             mpos1 = self.pos_marker_2
             mpos2 = self.pos_marker_1
  
@@ -53,37 +56,79 @@ class trial:
 
         cross   = visual.TextStim(self.window, color=-1, colorSpace='rgb', text='+', pos=self.pos_fixcross)
 
-        if self.flip[1] == 0:
+        if self.flip[1] == 1:
             spos1 = self.pos_stim_1
             spos2 = self.pos_stim_2
-        elif self.flip[1] == 1:
+        elif self.flip[1] == -1:
             spos1 = self.pos_stim_2
             spos2 = self.pos_stim_1
         
 #define stimuli here as stim_1 and stim_2
         print self.name
         if self.name=='dot':
-            direction = 0
-            stim_1 = visual.TextStim(self.window, text='dotstim1', pos=spos1)
-            stim_2 = visual.TextStim(self.window, text='dotstim2', pos=spos2)
+            #difficulty and direction must be set up
+            dot_direction_1, dot_direction_2 = 0, 0
+            dot_coherence_1, dot_coherence_2 = self.dif_stim_1, self.dif_stim_2
+
+            stim_1 = visual.DotStim(
+                    #constant parameters for dot motion
+                    self.window, color=(1.0,1.0,1.0), nDots=500, fieldShape='circle', fieldSize=0.7, 
+                    dotLife=100, signalDots='same', noiseDots='direction', speed=0.002, 
+                    #variable parameters for dot motion
+                    coherence=dot_coherence_1,
+                    dir=dot_direction_1,
+                    fieldPos=spos1
+                    )
+            stim_2 = visual.DotStim(
+                    #constant parameters for dot motion
+                    self.window, color=(1.0,1.0,1.0), nDots=500, fieldShape='circle', fieldSize=0.7,
+                    dotLife=100, signalDots='same', noiseDots='direction', speed=0.002, 
+                    #variable parameters for dot motion
+                    coherence=dot_coherence_2,
+                    dir=dot_direction_2,
+                    fieldPos=spos2
+                    )
+            dot_timer = core.CountdownTimer(self.time_stimulus)
+            while (dot_timer.getTime()>=0):
+                stim_1.draw()
+                stim_2.draw()
+                cross.draw()
+                marker_1.draw()
+                marker_2.draw()
+                self.window.flip()
+                core.wait(0.01)
+                
+
 
         elif self.name=='audio':
             stim_1 = visual.TextStim(self.window, text='audiostim1', pos=spos1)
             stim_2 = visual.TextStim(self.window, text='audiostim2', pos=spos2)
+            marker_1.draw()
+            marker_2.draw()
+            stim_1.draw()
+            stim_2.draw()
+            cross.draw()
+
 
         elif self.name=='math':
             stim_1 = visual.TextStim(self.window, text='mathstim1', pos=spos1)
             stim_2 = visual.TextStim(self.window, text='mathstim2', pos=spos2)
+            marker_1.draw()
+            marker_2.draw()
+            stim_1.draw()
+            stim_2.draw()
+            cross.draw()
+
 
         else :
             stim_1 = visual.TextStim(self.window, text='someotherstim1', pos=spos1)
             stim_2 = visual.TextStim(self.window, text='someotherstim2', pos=spos2)
+            marker_1.draw()
+            marker_2.draw()
+            stim_1.draw()
+            stim_2.draw()
+            cross.draw()
 
-        marker_1.draw()
-        marker_2.draw()
-        stim_1.draw()
-        stim_2.draw()
-        cross.draw()
 
 #Delay slide without interactivity
 
@@ -95,10 +140,10 @@ class trial:
 #Decision slide presented during recording of user input
 
     def define_slide_C(self):
-        if self.flip[0] == 0:
+        if self.flip[2] == 1:
             mpos1 = self.pos_marker_1
             mpos2 = self.pos_marker_2
-        elif self.flip[0] == 1:
+        elif self.flip[2] == -1:
             mpos1 = self.pos_marker_2
             mpos2 = self.pos_marker_1
         marker_1= visual.ImageStim(self.window, image=globvar.file_marker_1, pos=mpos1)
@@ -119,30 +164,54 @@ class trial:
 
 #Method to run trial i.e. define slides, show for certain time, record user input. Store data.
 
-    def run_trail(self):
-        self.define_slide_A()
-        self.window.flip()
-        core.wait(self.time_stimulus)
+    def run_trial(self):
+        #start time counter for the trial
+        time = core.MonotonicClock()
+        #get time of presentation of task slide from time counter
+        self.tA = time.getTime()
+        #present stimulus slide for time self.time_stimulus
+        self.present_slide_A()
+        #present task slide
+        #delay slide
         self.define_slide_B()
+        self.tB = time.getTime()
         self.window.flip()
         core.wait(self.time_delay_1)
+        #response slide
+        #clear events: empty user input list before response is recorded
+        event.clearEvents()
         self.define_slide_C()
+        #get start time
+        self.tC = time.getTime()
+        timer = core.CountdownTimer(self.time_decision)
+        #present response slide
         self.window.flip()
-        core.wait(self.time_decision)
-#record keystroke here
-        self.input_stack = event.getKeys(keyList=['left', 'right'] ,timeStamped=True)
-#did user make input or sleep?        
-        if np.shape(self.input_stack)[0] > 0: #input
-            tmp1, tmp2 = self.input_stack[0]
-#parse user input to numbers (binary: left=1, right=0)
-            if tmp1 == 'left':
-                self.user_input = [1,tmp2,1]
-            elif tmp1 == 'right':
-                self.user_input = [0,tmp2,1]
-        elif np.shape(self.input_stack)[0] == 0: #sleep
-            self.user_input = [0,0,0]
+        #wait for time_decision seconds for user input
+        #record keystroke here as [input==True, key, rt]
+        self.user_input = []
+        user_active = False
+        while timer.getTime()>=0:
+            for key in event.getKeys(keyList=['left','right']):
+                rt = time.getTime()-self.tC
+                if (self.flip[0]*self.flip[1]*self.flip[2]) == 1:
+                    if key == 'left':
+                        user_choice_dif = 'easy'
+                    elif key == 'right':
+                        user_choice_dif = 'hard'
+                elif (self.flip[0]*self.flip[1]*self.flip[2]) == -1:
+                    if key == 'left':
+                        user_choice_dif = 'hard'
+                    elif key == 'right':
+                        user_choice_dif = 'easy'
+                self.user_input = [1,key,rt,user_choice_dif]
+                user_active = True
+                break
+            if user_active == False:
+                self.user_input = [0,'none',-78,'none']
+        #draw baseline slide
         self.define_slide_D()
+        self.tD = time.getTime()
         self.window.flip()
         core.wait(self.time_delay_2)
-
+        self.tEnd = time.getTime()
 
--- trial_structure.py
+++ trial_structure.py
@@ -1,6 +1,7 @@
 import numpy as np
 from psychopy import core, visual, event, logging
-from classes import trial
+from trial_classes import trial
+import random
 import globvar
 logging.console.setLevel(logging.CRITICAL)
 
@@ -27,41 +28,44 @@ print 'trial setup'
 timing = np.zeros((number_of_trials,4))
 timing[:,0:4] = [3,2,1,2]
 for i in range(number_of_trials):
-    timing[i,1] = np.random.randint(1,5)
-    timing[i,3] = np.random.randint(2,5)
+    timing[i,0] = np.random.randint(1,5)
+    timing[i,2] = np.random.randint(2,5)
 #adjust means of timing for delay and baseline slides such 
-Estimated_delay_time  = sum(timing[:,1])/number_of_trials
+Estimated_delay_time  = sum(timing[:,0])/number_of_trials
+M   = int(number_of_trials*(Estimated_delay_time - desired_delay_average))
 n=1
-print Estimated_delay_time*number_of_trials, M
+print Estimated_delay_time, M
 while n<M:
+    #pick random time in the trial sequence
     i = np.random.randint(number_of_trials-1)
-    if timing[i,1]-1!=timing[i+1,1] and timing[i,1]-1!=timing[i+1,1] and timing[i,1]-1>0:
-        timing[i,1]-=1
+    #only change the value, if it is not equal to the time before and after
+    if timing[i,0]-1!=timing[i+1,0] and timing[i,0]-1!=timing[i+1,0] and timing[i,0]-1>0:
+        timing[i,0]-=1
         n+=1
 
-Estimated_baseline_time  = sum(timing[:,3])/number_of_trials
+Estimated_baseline_time  = sum(timing[:,2])/number_of_trials
 M   = int(number_of_trials*(Estimated_baseline_time - desired_baseline_average))
 n=1
 while n<M:
     i = np.random.randint(number_of_trials-1)
-    if timing[i,3]-1!=timing[i+1,3] and timing[i,3]-1!=timing[i+1,3] and timing[i,3]-2>0:
-        timing[i,1]-=1
+    if timing[i,2]-1!=timing[i+1,2] and timing[i,2]-1!=timing[i+1,2] and timing[i,2]-2>0:
+        timing[i,2]-=1
         n+=1
- 
-#difficulties for the current trial
+
+#difficulties for the current trial in the form [easy, hard] 
 difficulties =  np.zeros((number_of_trials,2))
-difficulties[:,0:2] = [0.1, 0.3]
+difficulties[:,0:2] = [1, 0.1]
 
 #deviation from standard order of elements on slide A and C
 inversions = np.zeros((number_of_trials,3))
 inversions[:,0:3] = [0,0,0]
 for i in range(number_of_trials):
-    inversions[i,0] = np.random.randint(0,1)
-    inversions[i,1] = np.random.randint(0,1)
-    inversions[i,2] = np.random.randint(0,1)
+    inversions[i,0] = random.choice([-1,1])
+    inversions[i,1] = random.choice([-1,1])
+    inversions[i,2] = random.choice([-1,1])
 
-print 'mean delay time is ', sum(timing[:,1])/number_of_trials
-print 'mean baseline time is ', sum(timing[:,3])/number_of_trials
+print 'mean delay time is ', sum(timing[:,0])/number_of_trials
+print 'mean baseline time is ', sum(timing[:,2])/number_of_trials
 
 #-----------------------------------------------------------------------------
 
@@ -71,22 +75,25 @@ print 'mean baseline time is ', sum(timing[:,3])/number_of_trials
 #-----------------------------------------------------------------------------
 win = visual.Window()
 
+output_file = open('choice_study_results.txt','w')
 
 for i in range(0,number_of_trials):
 
     current_trial = trial("dot", win, timing[i,:], spacing, difficulties[i,:], inversions[i,:])
-    current_trial.run_trail()
+    current_trial.run_trial()
+#data from trial.getData() is kind, user_active[y,n], input_key, reaction_time, difficulty_chosen, t_slide_a, t_slide_b, t_slide_c, t_slide_d
+    print 'stimuli type is ', current_trial.name
+    print 'user difficulty choice is ', current_trial.user_input[3]
+    print 'user reaction time is ', current_trial.user_input[2]
 #save data from each trial, timing of slides, user input and inversions of stimuli
-    output[i,0:4]   = timing[i,:]
-    output[i,4:7]   = current_trial.user_input
-    output[i,7:10]  = inversions[i,:]
+    print>>output_file, current_trial.getData() 
+    del current_trial
 
 win.close()
+output_file.close()
 #-----------------------------------------------------------------------------
 
 
 #-----------------------------------------------------------------------------
 #evaluate user input and save relevant data
 #-----------------------------------------------------------------------------
-
-np.savetxt('experiment_output_data.tsv', output)
