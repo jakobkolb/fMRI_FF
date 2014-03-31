a16698d0290494ea38f3bba09fe639f85e3770ea
jakobkolb <jkolb@physik.hu-berlin.de>
2014-03-31 17:46:22 +0200

    auditory trial working

--- trial_parameters.py
+++ trial_parameters.py
@@ -3,8 +3,8 @@
 #this file offers some general setting options
 
 #paths for marker files. relative paths must be given, if files are not in the same folder
-file_marker_1  = 'star.tif'
-file_marker_2  = 'square.tif'
+file_marker_1  = 'star.jpg'
+file_marker_2  = 'square.jpg'
 #values for reward for easy and hard tasks in the form [min_reward,max_reward] in Euro
 reward_hard = [0.5,1]          
 reward_easy = [0,0.5]
